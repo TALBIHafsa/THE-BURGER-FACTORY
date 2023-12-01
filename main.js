b5b1e9cf91cd4e3f4c9534792da84009c58d6ca6
TALBIHafsa <hafsatllb@gmail.com>
2023-12-02 00:59:06 +0100

    added the choices images within the choices bar and drew them in css

--- main.js
+++ main.js
@@ -6,59 +6,18 @@ window.addEventListener('scroll',()=>{
 });
 
 
-// // Add an event listener to the sauce icons
-// document.getElementById('sauce-icon').addEventListener('click', function() {
-//     var table = document.getElementById('sauce-choices');
-//     if (table.style.display === 'none') {
-//       table.style.display = 'block';  // Show the table when the icon is clicked
-//     } else {
-//       table.style.display = 'none';  // Hide the table if it's already visible
-//     }
-//   });
-// Add an event listener to the meat icons
-//   document.getElementById('meat-icon').addEventListener('click', function() {
-//     var table = document.getElementById('meat-choices');
-//     if (table.style.display === 'none') {
-//       table.style.display = 'block';  // Show the table when the icon is clicked
-//     } else {
-//       table.style.display = 'none';  // Hide the table if it's already visible
-//     }
-//   });
-// Add an event listener to the toppings icons
-//   document.getElementById('toppings-icon').addEventListener('click', function() {
-//     var table = document.getElementById('toppings-choices');
-//     if (table.style.display === 'none') {
-//       table.style.display = 'block';  // Show the table when the icon is clicked
-//     } else {
-//       table.style.display = 'none';  // Hide the table if it's already visible
-//     }
-//   });
-
-//   document.getElementById('cheese-icon').addEventListener('click', function() {
-//     var table = document.getElementById('cheese-choices');
-//     if (table.style.display === 'none') {
-//       table.style.display = 'block';  // Show the table when the icon is clicked
-//     } else {
-//       table.style.display = 'none';  // Hide the table if it's already visible
-//     }
-//   });
-
-
-
-
-
 
 //Add event listeners to the icons
 var icons = document.getElementsByClassName('icon');
 for (var i = 0; i < icons.length; i++) {
   icons[i].addEventListener('click', function() {
     var type = this.getAttribute('data-type');
-    var tables = document.getElementsByClassName('choices');
-    for (var j = 0; j < tables.length; j++) {
-      if (tables[j].id === type + '-choices') {
-        tables[j].style.display = 'block';  // Show the table corresponding to the clicked icon
+    var choices = document.getElementsByClassName('choices');
+    for (var j = 0; j < choices.length; j++) {
+      if (choices[j].id === type + '-choices') {
+        choices[j].style.display = 'flex';  // Show the table corresponding to the clicked icon
       } else {
-        tables[j].style.display = 'none';  // Hide the other tables
+        choices[j].style.display = 'none';  // Hide the other tables
       }
     }
   });
